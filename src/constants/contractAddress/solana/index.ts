68c49547bddfebfbb9ea611fad7e9655749bc093
Nguyen Duy <ichduy188@gmail.com>
2025-02-14 21:56:33 +0700

    Update lending

--- src/constants/contractAddress/solana/index.ts
+++ src/constants/contractAddress/solana/index.ts
@@ -2,7 +2,7 @@ import { PublicKey } from '@solana/web3.js';
 
 export const ctrAdsSolana = {
   //3APJcbC2iHEFGv4y6a8Fi5nQ5u75ML85TQreSr7cpRDs
-  lending: new PublicKey('5vLjP1n7CwibWWJbcywvGjoN3D3KPdWTXgdnt36cTHzC'),
+  lending: new PublicKey('3APJcbC2iHEFGv4y6a8Fi5nQ5u75ML85TQreSr7cpRDs'),
   oracle: new PublicKey('95Af7sxjVyunTTikaJQymK3rVwDtEVBtbnStXGMSR9wE'),
   vault: new PublicKey('BCaAj6hcBtKYVKUm1RnEyvE29Y1iu5ieLTq7CueN3vD3'),
 };
