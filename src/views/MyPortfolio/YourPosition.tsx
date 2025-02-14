aae22c653c7e3c594a7e4b4c459e115d99bba199
NguyenDucThinhh <thinhte0308@gmail.com>
2025-02-14 22:02:13 +0700

    update: ratio

--- src/views/MyPortfolio/YourPosition.tsx
+++ src/views/MyPortfolio/YourPosition.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from '@mui/material';
 import { useMemo } from 'react';
 import { BoxCustom } from 'src/components/General/BoxCustom/BoxCustom';
 import ValueWithStatus from 'src/components/General/ValueWithStatus/ValueWithStatus';
+import { findTokenInfoByToken } from 'src/constants/tokens/solana-ecosystem/mapNameToInfoSolana';
 import useQueryAllTokensPrice from 'src/hooks/useQueryAllTokensPrice';
 import useQueryDepositValue from 'src/hooks/useQueryHook/queryMyPortfolio/useQueryDepositValue';
 import useQueryYourBorrow from 'src/hooks/useQueryHook/queryMyPortfolio/useQueryYourBorrow';
@@ -9,13 +10,22 @@ import { formatNumber } from 'src/utils/format';
 import SliderCustom from './components/SliderCustom';
 
 export default function YourPosition() {
-  const { data: depositValueData, status } = useQueryDepositValue();
-  const { data: yourBorrowData } = useQueryYourBorrow();
-  const { data: tokensPrice } = useQueryAllTokensPrice();
+  const { data: depositValueData, status: statusQueryDepositValue } = useQueryDepositValue();
+  const { data: yourBorrowData, status: statusQueryYourBorrow } = useQueryYourBorrow();
+  const { data: tokensPrice, status: statusQueryAllTokensPrice } = useQueryAllTokensPrice();
 
   const totalDepositValue = useMemo(() => {
     if (depositValueData && Object.keys(depositValueData).length > 0 && tokensPrice) {
       return Object.entries(depositValueData).reduce((a, [k, v]) => a + Number(v) * Number(tokensPrice[k].price), 0);
+    }
+  }, [depositValueData, tokensPrice]);
+
+  const totalDepositValueRatio = useMemo(() => {
+    if (depositValueData && Object.keys(depositValueData).length > 0 && tokensPrice) {
+      return Object.entries(depositValueData).reduce(
+        (a, [k, v]) => a + Number(v) * Number(tokensPrice[k].price * Number(findTokenInfoByToken(k) ? findTokenInfoByToken(k)?.ratio : 1)),
+        0
+      );
     }
   }, [depositValueData, tokensPrice]);
 
@@ -32,7 +42,7 @@ export default function YourPosition() {
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
           <Typography variant="subtitle2">Collateral Deposited</Typography>
           <ValueWithStatus
-            status={[status]}
+            status={[statusQueryDepositValue, statusQueryYourBorrow, statusQueryAllTokensPrice]}
             value={
               <Typography variant="body2" sx={{ fontWeight: 700 }}>
                 {formatNumber(totalDepositValue, { fractionDigits: 2, prefix: '$' })}
@@ -46,17 +56,17 @@ export default function YourPosition() {
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
           <Typography variant="subtitle2">Borrow</Typography>
           <ValueWithStatus
-            status={[status]}
+            status={[statusQueryDepositValue, statusQueryYourBorrow, statusQueryAllTokensPrice]}
             value={
               <Typography variant="body2" sx={{ fontWeight: 700 }}>
                 {totalDepositValue != undefined
-                  ? formatNumber(totalDepositValue * 0.3, { fractionDigits: 2, prefix: '$' })
+                  ? formatNumber(totalDepositValueRatio, { fractionDigits: 2, prefix: '$' })
                   : formatNumber(totalDepositValue, { fractionDigits: 2, prefix: '$' })}
               </Typography>
             }
           />
         </Box>
-        <SliderCustom value={totalYourBorrowValue} maxValue={totalDepositValue && Number(totalDepositValue) * 0.3} />
+        <SliderCustom value={totalYourBorrowValue} maxValue={totalDepositValue && Number(totalDepositValueRatio)} />
       </Box>
     </BoxCustom>
   );
