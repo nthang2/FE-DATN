b0e43fb60bca0d7b3e79d563ae27563eb9aabe3b
Nguyen Duy <ichduy188@gmail.com>
2025-02-14 17:22:49 +0700

    Feature: Update menu

--- src/layout/menu.ts
+++ src/layout/menu.ts
@@ -12,8 +12,8 @@ export const menu: TMenu[] = [
     title: 'Borrow',
     url: '/',
   },
-  {
-    title: 'Vaults',
-    url: '/vaults',
-  },
+  // {
+  //   title: 'Vaults',
+  //   url: '/vaults',
+  // },
 ];
