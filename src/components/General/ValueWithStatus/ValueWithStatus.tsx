1a74dba4c21eab3dcc063758715097bd1336c3ff
Nguyen Duy <ichduy188@gmail.com>
2025-02-17 15:57:09 +0700

    Bugfix: Fix loading state

--- src/components/General/ValueWithStatus/ValueWithStatus.tsx
+++ src/components/General/ValueWithStatus/ValueWithStatus.tsx
@@ -1,11 +1,17 @@
-import { Box, Skeleton } from '@mui/material';
+import { Box, Skeleton, SxProps } from '@mui/material';
 import NAValue from 'src/components/StatusData/NAValue';
 
-export default function ValueWithStatus({ status, value }: { status: Array<'pending' | 'success' | 'error'>; value?: React.ReactNode }) {
+interface IProps {
+  status: Array<'pending' | 'success' | 'error'>;
+  value?: React.ReactNode;
+  skeletonStyle?: SxProps;
+}
+
+export default function ValueWithStatus({ status, value, skeletonStyle }: IProps) {
   return (
     <Box className="flex-center">
       {status.find((item) => item == 'pending') ? (
-        <Skeleton variant="text" sx={{ width: '60px', height: '24px' }} />
+        <Skeleton variant="text" sx={{ width: '60px', height: '24px', ...skeletonStyle }} />
       ) : status.find((item) => item == 'error') ? (
         <NAValue />
       ) : (
