879a16b2b97bb234f264efb1bcbb5cd794c7eba5
NguyenDucThinhh <thinhte0308@gmail.com>
2025-02-18 13:38:35 +0700

    chore: update range health factor

--- src/constants/contractAddress/solana/index.ts
+++ src/constants/contractAddress/solana/index.ts
@@ -4,7 +4,7 @@ import { NETWORK } from 'src/constants';
 export const vaultProgramId =
   NETWORK === 'devnet' ? 'BCaAj6hcBtKYVKUm1RnEyvE29Y1iu5ieLTq7CueN3vD3' : 'GtTBznMzJiND5niGV3az8LLnUbNLphqkJVHDKuCt2VYy';
 export const lendingProgramId =
-  NETWORK === 'devnet' ? 'HtSzqFJCFwpRKarN8PBfk2kEXwUzJLUJvhiG52sKpZWZ' : '3APJcbC2iHEFGv4y6a8Fi5nQ5u75ML85TQreSr7cpRDs';
+  NETWORK === 'devnet' ? '7yqKNNqAy6Xg9wxSYzYgi2j7dXpSNJVE2SorLJp8ZAEY' : '3APJcbC2iHEFGv4y6a8Fi5nQ5u75ML85TQreSr7cpRDs';
 
 export const ctrAdsSolana = {
   //3APJcbC2iHEFGv4y6a8Fi5nQ5u75ML85TQreSr7cpRDs
--- src/views/MyPortfolio/YourPosition.tsx
+++ src/views/MyPortfolio/YourPosition.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import { TokenName } from 'crypto-token-icon';
 import { useMemo } from 'react';
 import { BoxCustom } from 'src/components/General/BoxCustom/BoxCustom';
 import ValueWithStatus from 'src/components/General/ValueWithStatus/ValueWithStatus';
@@ -6,26 +7,33 @@ import { findTokenInfoByToken } from 'src/constants/tokens/solana-ecosystem/mapN
 import useQueryAllTokensPrice from 'src/hooks/useQueryAllTokensPrice';
 import useQueryDepositValue from 'src/hooks/useQueryHook/queryMyPortfolio/useQueryDepositValue';
 import useQueryYourBorrow from 'src/hooks/useQueryHook/queryMyPortfolio/useQueryYourBorrow';
+import useStakedInfo from 'src/hooks/useQueryHook/queryVault/useStakedInfo';
+import useSolanaBalanceToken from 'src/states/wallets/solana-blockchain/hooks/useSolanaBalanceToken';
+import useSummarySolanaConnect from 'src/states/wallets/solana-blockchain/hooks/useSummarySolanaConnect';
 import { formatNumber } from 'src/utils/format';
 import SliderCustom from './components/SliderCustom';
 
 export default function YourPosition() {
+  const { address } = useSummarySolanaConnect();
   const { data: depositValueData, status: statusQueryDepositValue } = useQueryDepositValue();
   const { data: yourBorrowData, status: statusQueryYourBorrow } = useQueryYourBorrow();
   const { data: tokensPrice, status: statusQueryAllTokensPrice } = useQueryAllTokensPrice();
+  const { data: dataStakedInfo, status: statusStakedInfo } = useStakedInfo();
+  const { balance: balanceUSDAI } = useSolanaBalanceToken(address, TokenName.USDAI);
 
   const totalDepositValue = useMemo(() => {
     if (depositValueData && Object.keys(depositValueData).length > 0 && tokensPrice) {
-      return Object.entries(depositValueData).reduce((a, [k, v]) => a + Number(v) * Number(tokensPrice[k].price), 0);
+      return Object.entries(depositValueData).reduce((a, [k, v]) => a + Number(v) * Number(tokensPrice[k]?.price ?? 1), 0);
     }
   }, [depositValueData, tokensPrice]);
 
   const totalDepositValueRatio = useMemo(() => {
     if (depositValueData && Object.keys(depositValueData).length > 0 && tokensPrice) {
-      return Object.entries(depositValueData).reduce(
-        (a, [k, v]) => a + Number(v) * Number(tokensPrice[k].price * Number(findTokenInfoByToken(k) ? findTokenInfoByToken(k)?.ratio : 1)),
-        0
-      );
+      return Object.entries(depositValueData).reduce((a, [k, v]) => {
+        const total =
+          a + Number(v) * Number(tokensPrice[k]?.price * Number(findTokenInfoByToken(k) ? findTokenInfoByToken(k)?.ratio ?? 0.3 : 1));
+        return total;
+      }, 0);
     }
   }, [depositValueData, tokensPrice]);
 
@@ -74,6 +82,24 @@ export default function YourPosition() {
           status={[statusQueryDepositValue, statusQueryYourBorrow, statusQueryAllTokensPrice]}
           value={totalYourBorrowValue}
           maxValue={totalDepositValue && Number(totalDepositValueRatio)}
+        />
+      </Box>
+      <Box sx={{ mb: 4, mt: 8 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Typography variant="subtitle2">Stake</Typography>
+          <ValueWithStatus
+            status={[statusQueryDepositValue, statusQueryYourBorrow, statusQueryAllTokensPrice]}
+            value={
+              <Typography variant="body2" sx={{ fontWeight: 700 }}>
+                {formatNumber(balanceUSDAI.toString(), { prefix: '$' })}
+              </Typography>
+            }
+          />
+        </Box>
+        <SliderCustom
+          status={[statusStakedInfo]}
+          value={dataStakedInfo?.amount ? Number(dataStakedInfo?.amount) : 0}
+          maxValue={Number(balanceUSDAI.toString())}
         />
       </Box>
     </BoxCustom>
