3755670a630dc3e16b212c0a2b2a6b174d16f480
Nguyen Duy <ichduy188@gmail.com>
2025-02-24 18:28:53 +0700

    udpate router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,10 @@
 import { useRoutes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Borrow from './views/Borrow/Borrow';
-// import LiquidationLeaderboard from './views/LiquidationLeaderboard/LiquidationLeaderboard';
 import MyPortfolio from './views/MyPortfolio/MyPortfolio';
 import NotFound from './views/NotFound/NotFound';
 import Vaults from './views/Vaults/Vaults';
-// import LiquidationLeaderboard from './views/LiquidationLeaderboard/LiquidationLeaderboard';
+import LiquidationLeaderboard from './views/LiquidationLeaderboard/LiquidationLeaderboard';
 
 export default function Routes() {
   return useRoutes([
@@ -16,7 +15,7 @@ export default function Routes() {
         { path: '/', element: <Borrow /> },
         { path: '/my-portfolio', element: <MyPortfolio /> },
         { path: '/vaults', element: <Vaults /> },
-        // { path: '/liquidation', element: <LiquidationLeaderboard /> },
+        { path: '/liquidation', element: <LiquidationLeaderboard /> },
       ],
     },
     {
