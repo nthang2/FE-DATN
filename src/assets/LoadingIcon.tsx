91649676ed371debfb98c9f4e771870224ed5d7e
Nguyen Duy <ichduy188@gmail.com>
2025-03-04 15:36:08 +0700

    Feature: Refetch price tokens every 30s

--- src/assets/LoadingIcon.tsx
+++ src/assets/LoadingIcon.tsx
@@ -1,5 +1,5 @@
 import { Box, BoxProps } from '@mui/material';
-import LoadingSvg from 'public/svgs/LoadingSvg.gif';
+import LoadingSvg from '/svgs/LoadingSvg.gif';
 
 const LoadingIcon = (props: BoxProps) => {
   return <Box component="img" alt="loading_svg" height={40} width={40} src={LoadingSvg} {...props} />;
