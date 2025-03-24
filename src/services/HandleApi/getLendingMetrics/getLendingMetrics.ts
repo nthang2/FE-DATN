399c048cfd86ba260b836c41cae7951439329d4a
Nguyen Duy <ichduy188@gmail.com>
2025-03-24 15:04:06 +0700

    Feature: Update Mint Banner Cross Mode

--- src/services/HandleApi/getLendingMetrics/getLendingMetrics.ts
+++ src/services/HandleApi/getLendingMetrics/getLendingMetrics.ts
@@ -6,3 +6,8 @@ export async function getLendingMetrics(): Promise<TLendingMetric> {
   const response = await axios.get(apiUrl.getLendingMetrics());
   return response.data as TLendingMetric;
 }
+
+export async function getLendingMetricsCrossMode(): Promise<TLendingMetric> {
+  const response = await axios.get(apiUrl.getLendingMetricsCrossMode());
+  return response.data as TLendingMetric;
+}
