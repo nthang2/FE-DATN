858655fc4c89a7168d840fefb7ab304ad65fc60e
Nguyen Duy <ichduy188@gmail.com>
2025-07-02 14:21:27 +0700

    Feature: Implement maxLiquidationThreshold calculation and integrate it into LTVSection and CustomMark components for improved liquidation value display

--- src/views/Borrow/constant.ts
+++ src/views/Borrow/constant.ts
@@ -1,14 +1,12 @@
 import { listTokenAvailable } from 'src/constants/tokens/solana-ecosystem/mapNameToInfoSolana';
+import { TokenName } from 'src/libs/crypto-icons';
 
 export const marks = [
   {
-    value: 37,
+    value: 1,
   },
   {
-    value: 51,
-  },
-  {
-    value: 80,
+    value: 35,
   },
 ];
 
@@ -31,4 +29,13 @@ export const defaultBorrowValue = {
   address: Object.values(listTokenAvailable)[0]?.address as string,
   value: '0',
   price: 0,
+};
+
+export const liquidationThreshold = {
+  [TokenName.MAX]: 0.35,
+  [TokenName.USDC]: 0.98,
+  [TokenName.ORAI]: 0.45,
+  [TokenName.SOL]: 0.65,
+  [TokenName.VIRTUAL]: 0.35,
+  [TokenName.scORAI]: 0.45,
 };
