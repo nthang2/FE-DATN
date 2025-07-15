eba23543ee9068cc7e8551e8686994f5a9fe1366
Nguyen Duy <ichduy188@gmail.com>
2025-07-15 09:17:28 +0700

    Feature: Update SwapInfo component to display of swap details

--- src/views/SwapToken/SwapForm.tsx
+++ src/views/SwapToken/SwapForm.tsx
@@ -149,7 +149,7 @@ export default function SwapForm() {
         </Stack>
       </Stack>
 
-      <SwapInfo />
+      <SwapInfo selectedToken={selectedToken} amount={selectTokenAmount} />
 
       <ButtonLoading
         fullWidth
--- src/views/SwapToken/SwapInfo.tsx
+++ src/views/SwapToken/SwapInfo.tsx
@@ -1,21 +1,48 @@
 import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
 import { Stack, Typography } from '@mui/material';
 import { MinimumReceivedIcon, PriceImpactIcon } from 'src/assets/icons';
+import { findTokenInfoByToken } from 'src/constants/tokens/solana-ecosystem/mapNameToInfoSolana';
 
-const SwapInfo = () => {
+interface Props {
+  selectedToken: string;
+  amount: number;
+}
+
+const SwapInfo = (props: Props) => {
+  const { selectedToken, amount } = props;
+  const tokenInfo = findTokenInfoByToken(selectedToken);
+
   return (
     <Stack flexDirection="column" gap={2} borderTop="1px solid #323326" borderBottom="1px solid #323326" py={2}>
-      <Typography display="flex" alignItems="center" gap={1} variant="body1" color="text.secondary">
-        <MinimumReceivedIcon /> Minimum received after slippage (0.50%)
-      </Typography>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography display="flex" alignItems="center" gap={1} variant="body1" color="text.secondary">
+          <MinimumReceivedIcon /> Minimum received after slippage (0.50%)
+        </Typography>
 
-      <Typography display="flex" alignItems="center" gap={1} variant="body1" color="text.secondary">
-        <PriceImpactIcon /> Price impact (0.50%)
-      </Typography>
+        <Typography variant="body1" color="text.secondary">
+          {amount} {tokenInfo?.symbol}
+        </Typography>
+      </Stack>
 
-      <Typography display="flex" alignItems="center" gap={1} variant="body1" color="text.secondary">
-        <LocalGasStationIcon /> Network Fee
-      </Typography>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography display="flex" alignItems="center" gap={1} variant="body1" color="text.secondary">
+          <PriceImpactIcon /> Price impact (0.50%)
+        </Typography>
+
+        <Typography variant="body1" color="text.secondary">
+          0.00%
+        </Typography>
+      </Stack>
+
+      <Stack direction="row" justifyContent="space-between">
+        <Typography display="flex" alignItems="center" gap={1} variant="body1" color="text.secondary">
+          <LocalGasStationIcon /> Network Fee
+        </Typography>
+
+        <Typography variant="body1" color="text.secondary">
+          ~$0.00
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
