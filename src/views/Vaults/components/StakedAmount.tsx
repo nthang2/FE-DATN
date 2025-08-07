7556bca59f6efc66b3dc9805bec27df506e7aed5
Nguyen Duy <ichduy188@gmail.com>
2025-08-07 14:03:14 +0700

    Chore: Update display Staked Amount

--- src/views/Vaults/components/StakedAmount.tsx
+++ src/views/Vaults/components/StakedAmount.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import ValueWithStatus from 'src/components/General/ValueWithStatus/ValueWithStatus';
 import useStakedInfo from 'src/hooks/useQueryHook/queryVault/useStakedInfo';
-import { roundNumber } from 'src/utils/format';
+import { formatNumber, roundNumber } from 'src/utils/format';
 
 const StakedAmount = () => {
   const { stakeInfo, status } = useStakedInfo();
@@ -26,7 +26,7 @@ const StakedAmount = () => {
           status={[status]}
           value={
             <Typography variant="h2" fontWeight={700} fontSize="42px">
-              ${roundNumber(stakeInfo?.amount || 0, 4)}
+              ${formatNumber(stakeInfo?.amount || 0, { fractionDigits: 4, delimiter: ',' })}
             </Typography>
           }
           skeletonStyle={{ bgcolor: '#b7b4b4', height: '60px', width: '50%' }}
