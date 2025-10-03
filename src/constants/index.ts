9cee5cb43fb56922d86769d2fd79a12bebcd96fe
Nguyen Duy <ichduy188@gmail.com>
2025-10-03 14:00:58 +0700

    refactor: update default RPC URL to use clusterApiUrl for devnet in constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,12 @@
+import { clusterApiUrl } from '@solana/web3.js';
+
 export const FontOxanium = "'Oxanium', serif";
 
 export const ProjectID_WalletConnect = '349c73495596fe0938c2557979bddc8c';
 
 export const NETWORK: 'mainnet' | 'devnet' = 'devnet'; //change to mainnet later
 
-export const defaultRpc =
-  NETWORK === 'devnet'
-    ? 'https://rpc.ankr.com/solana_devnet/8cb7e20d1f00146c56101888eb894779d8e88f455eb61fb3e772f3ccddc66661'
-    : 'https://solana-woker.distilled.ai/';
+export const defaultRpc = NETWORK === 'devnet' ? clusterApiUrl('devnet') : 'https://solana-woker.distilled.ai/';
 
 export const MAXIMUM_ALLOW = '999999999999999999999999999999';
 
