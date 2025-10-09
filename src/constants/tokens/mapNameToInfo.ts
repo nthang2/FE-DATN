ec22f00072687827de0a96cb71e9255cb9042abe
Nguyen Duy <ichduy188@gmail.com>
2025-10-09 14:17:51 +0700

    Feature: Integrate universal token info mapping for Solana and Ethereum in DepositPreview component

--- src/constants/tokens/mapNameToInfo.ts
+++ src/constants/tokens/mapNameToInfo.ts
@@ -6,6 +6,8 @@ import { listTokenAvailable as listTokenAvailableEVM } from './evm-ecosystem/map
 import { TokenName } from 'src/libs/crypto-icons';
 import { SolanaDevnetTokenInfo } from './solana-ecosystem/solana-devnet/SolanaDevnetTokenInfo';
 import { EthereumChainTokenInfo } from './evm-ecosystem/list-tokens/ethereum/EthereumChainTokenInfo';
+import { mapNameToInfoSolana as mapNameToInfoSOL } from './solana-ecosystem/mapNameToInfoSolana';
+import { mapNameToInfoEthereum as mapNameToInfoEVM } from './evm-ecosystem/mapNameToInfoEthereum';
 
 export const findTokenInfoByToken = (token: string, network: string) => {
   if (network.toLowerCase() === mapNameNetwork.solana.name.toLowerCase()) {
@@ -20,5 +22,13 @@ export const listTokenAvailableUniversal = (network: string) => {
     return listTokenAvailableSOL as Record<TokenName, SolanaDevnetTokenInfo | undefined>;
   } else if (network.toLowerCase() === mapNameNetwork.ethereum.name.toLowerCase()) {
     return listTokenAvailableEVM as Record<TokenName, EthereumChainTokenInfo | undefined>;
+  }
+};
+
+export const mapNameToInfoUniversal = (network: string) => {
+  if (network.toLowerCase() === mapNameNetwork.solana.name.toLowerCase()) {
+    return mapNameToInfoSOL as Record<TokenName, SolanaDevnetTokenInfo | undefined>;
+  } else if (network.toLowerCase() === mapNameNetwork.ethereum.name.toLowerCase()) {
+    return mapNameToInfoEVM as Record<TokenName, EthereumChainTokenInfo | undefined>;
   }
 };
