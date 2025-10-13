150bc9679d080c8f0d65b1ca6d6443b798c8e3f0
NguyenDucThinhh <thinhte0308@gmail.com>
2025-10-13 11:18:59 +0700

    update: filter tx history

--- src/services/HandleApi/getMyPortfolioUniversal/getTransactionsHistory.tsx
+++ src/services/HandleApi/getMyPortfolioUniversal/getTransactionsHistory.tsx
@@ -3,7 +3,9 @@ import { apiUrl } from 'src/services/apiUrl';
 import { TTransactionHistoryBody, TTransactonHistoryItem } from './type';
 
 export const getTransactionsHistory = async (body: TTransactionHistoryBody) => {
-  const resp = await axios.get<TTransactonHistoryItem>(apiUrl.getPortfolioCrossTransactionsHistory(body.walletAddress, body.chainId));
+  const resp = await axios.get<TTransactonHistoryItem>(
+    apiUrl.getPortfolioCrossTransactionsHistory(body.walletAddress, body.chainId, body.pageIndex, body.pageSize)
+  );
 
   return resp.data;
 };
