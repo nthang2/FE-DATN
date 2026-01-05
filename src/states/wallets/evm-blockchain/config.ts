099d88c4c491f37d78e3345941be3b6d0e4d0944
Nguyen Duy <ichduy188@gmail.com>
2026-01-05 15:29:22 +0700

    feat: Add sepolia chain configuration and integrate into wallet config

--- src/states/wallets/evm-blockchain/config.ts
+++ src/states/wallets/evm-blockchain/config.ts
@@ -1,8 +1,8 @@
 import { createConfig, http } from 'wagmi';
 // import { mainnet, sepolia } from 'wagmi/chains';
-import { sepolia } from 'wagmi/chains';
 import { metaMaskConnector } from './connectors/metamaskConnector';
 import { walletConnectConnector } from './connectors/walletConnectConnector';
+import { sepolia } from './chain/sepolia';
 
 export const config = createConfig({
   chains: [sepolia],
